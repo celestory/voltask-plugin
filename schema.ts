80803e4b6d99bfc839f3e8f7db5dedf818804ce4
JonathanPicques <jonathan.picques@gmail.com>
2023-12-01 18:35:06 +0100

    feat(schema): add any in schema

--- schema.ts
+++ schema.ts
@@ -1,23 +1,57 @@
-export type JSONSchema =
-    | {type: 'number'; default?: number}
-    | {type: 'string'; default?: string}
-    | {type: 'boolean'; default?: boolean}
-    | {type: 'object'; required?: readonly string[]; properties: Record<string, JSONSchema>; additionalProperties: boolean}
-    | {
-          type: 'array';
-          title?: string;
-          default?: unknown[];
-          items?: JSONSchema;
-          prefixItems?: JSONSchema[];
-          minItems?: number;
-          maxItems?: number;
-          uniqueItems?: boolean;
-      };
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type JSONSchemaAny = {
+    type?: never;
+    title?: string;
+    default?: never;
+};
+
+export type JSONSchemaNumber = {
+    type: 'number';
+    title?: string;
+    default?: number;
+};
+
+export type JSONSchemaString = {
+    type: 'string';
+    enum?: string[];
+    title?: string;
+    default?: string;
+};
+
+export type JSONSchemaBoolean = {
+    type: 'boolean';
+    title?: string;
+    default?: boolean;
+};
+
+export type JSONSchemaArray = {
+    type: 'array';
+    items?: JSONSchema;
+    title?: string;
+    default?: unknown[];
+    minItems?: number;
+    maxItems?: number;
+    prefixItems?: JSONSchema[];
+    uniqueItems?: boolean;
+};
+
+export type JSONSchemaObject = {
+    type: 'object';
+    title?: string;
+    default?: object;
+    required?: readonly string[];
+    properties: Record<string, JSONSchema>;
+    additionalProperties?: boolean;
+};
+
+export type JSONSchema = JSONSchemaAny | JSONSchemaNumber | JSONSchemaString | JSONSchemaBoolean | JSONSchemaArray | JSONSchemaObject;
 
 export type FromSchema<T> = T extends {type: 'number'}
     ? number
     : T extends {type: 'string'}
     ? string
+    : T extends {type: 'array'; items: infer P}
+    ? FromSchema<P>[]
     : T extends {type: 'object'; properties: infer P}
     ? {[K in keyof P]: FromSchema<P[K]>}
     : never;
